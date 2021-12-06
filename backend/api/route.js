2cde9b2e1ffead08639a29359ddbf87bab67bb83
Zakir Hossain <zakirsust2017@gmail.com>
2021-12-07 01:07:23 +0600

    Added the initial empty classes and functions

---
+++ backend/api/route.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import routeCtrl from './route.controller.js'
+
+const router = express.Router();
+
+
+router.route('/item')
+.get(routeCtrl.apiGetItem())
+.post(routeCtrl.apiPostItem())
+.delete(routeCtrl.apiDeleteItem())
+
+router.route('/user')
+.get(routeCtrl.apiGetUser())
+.post(routeCtrl.postUser())
+
+
+export default router;
