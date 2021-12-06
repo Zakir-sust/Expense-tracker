407b8c429495e4068e4ed4932b009da6d35d5219
Zakir Hossain <zakirsust2017@gmail.com>
2021-12-07 02:12:28 +0600

    Minor Change

---
+++ backend/api/route.controller.js
@@ -0,0 +1,26 @@
+import itemModel from '../db/item.js'
+
+
+class RouteCtrl{
+    static async apiAdduser(req,res){
+
+    }
+
+    static async apiPostUser(req,res){
+
+    }
+
+    static async apiAddItem(req,res){
+
+    }
+
+    static async apiDeleteItem(req,res){
+
+    }
+
+    static async apiPostItem(req,res){
+
+    }
+}
+
+export default RouteCtrl
