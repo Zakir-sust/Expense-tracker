f6b397f1d475f02e83c33666ac5aae51ad9d9010
Zakir Hossain <zakirsust2017@gmail.com>
2021-12-07 15:00:18 +0600

    replaced require by import

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,32 @@
-import itemRoute from './api/route.js'
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+// import itemRoute from "./api/route.js"
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port =  5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+// const uri = process.env.ATLAS_URI;
+const uri = "mongodb+srv://samba:ROOT@cluster0.atu6p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("Connection established")
 })
 
-app.use('/api/expense-tracker',itemRoute)
+// app.use('/api/expense-tracker',itemRoute)
 
 app.listen(port, () => {
     console.log(`Server is running on port : ${port}`);
 })
+
