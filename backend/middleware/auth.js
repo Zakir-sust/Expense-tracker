9723743022000b06ac248289b8523ba5d994a783
Zakir Hossain <zakirsust2017@gmail.com>
2021-12-08 02:56:07 +0600

    auth.js added

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken'
+import User from '../db/user.js'
+const auth = async(req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decode = await jwt.verify(token, 'thisisnewuser')
+        const user = await User.findOne({ _id: decode._id, 'tokens.token': token })
+        if (!user)
+            throw new Error()
+        req.token = token
+        req.user = user
+        next();
+    } catch (e) {
+        res.status(403).send({ error: "error in Authentication" })
+    }
+}
+
+export default auth;
